4be70aaf53bac40eb730397009d5164e7d19bbc5
nitharsan21 <nimalaluxman.nitharsan.sio@gmail.com>
2022-03-05 19:58:33 +0000

    infra dataset BigQuery

--- TerraformScript/backend_bucket_basic/main.tf
+++ TerraformScript/backend_bucket_basic/main.tf
@@ -4,5 +4,30 @@ resource "google_storage_bucket" "my_bucket" {
   force_destroy = true
 
   uniform_bucket_level_access = true
-  
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "insta2"
+  friendly_name               = "proto"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
 }
